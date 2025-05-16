25c6a52664c45bd4677dbf9c993ac0ea39055945
Nelton <neonewton@hotmail.com>
2025-05-16 11:45:56 +0800

    1145am

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-print("hello world")
+import keyword
 
+# print(keyword.kwlist)
+
+x = 100
+s='welcome'
+
+
+print (type(x)) #int
+print (type(100)) #int
+print (type(100.0)) #float
+print (type(s) ) #str

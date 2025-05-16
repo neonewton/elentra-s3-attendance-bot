3e8be7be31ca9ff52f7fbad7de2ed7b673bca6ce
Nelton <neonewton@hotmail.com>
2025-05-16 16:42:13 +0800

    1642pm

--- Basic/Day3.py
+++ Basic/Day3.py
@@ -13,3 +13,33 @@ print(list(range (2,10,2))) #[2, 4, 6, 8]
 print(list(range (10,1, -1))) #[10, 9, 8, 7, 6, 5, 4, 3, 2]
 
 print(list(range(-10, -5))) # [-10, -9, -8, -7, -6]
+
+#------------------------
+# conditional statements - if if..else
+# looping statements
+# jumping statements elif
+
+# print (1)
+# print (2)
+
+# range() function in python
+#------------------------
+# range (10) 0.....10
+# range (1,10) 1,........9
+# range (1,10,2) 1- starting point, 10 -ending point 2-increment
+
+# looping statemenets
+#------------------------
+# while loop
+# for loop
+
+# initilization
+# condition
+# incrementation
+
+i=1
+
+while i<=10:
+    print(i)
+    i+=1
+print ("done!!")

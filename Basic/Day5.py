955cedb5ea6ee02b601ad8cfc7390b5768446004
Nelton <neonewton@hotmail.com>
2025-05-20 12:10:35 +0800

    20250520_1210pm

---
+++ Basic/Day5.py
@@ -0,0 +1,87 @@
+import sys
+import os
+os.system('clear')
+
+"""
+# Examplel : creating set
+myset={"apple", "banana", "cherry"}
+print(myset) #{'banana', 'cherry', 'apple'}
+
+# Example2:_ Accessing items from_ set
+myset={"apple", "banana", "cherry"}
+for i in myset:
+    print(i)
+
+#Example3: value exists in set or not
+myset={"apple", "banana", "cherry"}
+print("banana" in myset) #True
+print("orange" in myset) #False
+
+#Example4: adding items to set
+# add(-add single item      #update()-add multiple items
+myset={"apple", "banana", "cherry"}
+myset.add ("orange") #(added randomly due to unordered nature of set)
+myset.update 
+(["mango", "grapes"])
+print(myset) 
+
+
+
+#Example5: find number of items in a set - leno
+myset={"apple", "banana", "cherry"}
+print(len(myset)) #3
+
+#Example6: remove item from set
+#remove() - remove item by value
+#discard() - remove item by value
+#pop() - remove last item
+
+# myset={"apple", "banana", "cherry"}
+# myset.remove("banana") #removes banana
+# print(myset) #{'cherry', 'apple'}
+
+# myset.discard("banana"
+# myset.discard("xyz") #will not through anany error
+# print(myset) 
+
+#Example8: joining 2 sets - union()
+set1={"a", "b" , "c"}
+set2={1,2,3}
+set3=set1.union (set2)
+print(set3) #{1, 2, 'b', 3, 'c', 'a'}
+
+#update()
+set1={"a", "b", "C"}
+set2={1,2,3}
+set1.update (set2)
+"""
+#Dictionary 
+
+mydict={10,20,30}
+print(mydict) #{10, 20, 30}
+
+mydic={
+"brand": "Hyudai",
+"model": "i10",
+"year": 2020
+}
+
+for i in mydic:
+print(1) # prints only keys from dictionary
+
+for i in mydic:
+print(mydic[i]) # prints only values from dictionary
+
+"""
+
+
+
+
+"""
+"""
+
+
+
+
+
+"""
